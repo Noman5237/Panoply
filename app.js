5fb7fff76dc6f61b622a0ca2923638aa92697a65
Noman^Alien#0637 <nomanalien5237@gmail.com>
2018-11-14 00:40:30 +0600

    Basic App Built Need a lot more work

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-window.addEventListener("DOMContentLoaded", function() {
-    console.log("Hello World!");
-    // navigator.spatialNavigationEnabled = true;
-    window.addEventListener('keydown', function(e){
+let mainOutput = '';
+
+window.addEventListener('DOMContentLoaded', function () {
+    
+    // src code
+    let src = document.getElementById('srcCode');
+    src.focus();
+    
+    // console
+    let output = document.getElementById('console');
+    
+    // tab space
+    let tabSpace = '   ';   // 3
+    
+    // todo where ot put the cursor in load up
+    
+    let editorOnFocus = true;
+    
+    window.addEventListener('keydown', function (e) {
         switch (e.key) {
-            case 'SoftRight':
-                // todo handle the help page
-                break;
             case 'SoftLeft':
-                // todo handle the More page
+                // tab was pressed
+                let start = src.selectionStart;
+                let end = src.selectionEnd;
+                let code = src.value;
+                
+                src.value = code.substring(0, start) + tabSpace + code.substring(end);
+                
+                src.selectionStart = src.selectionEnd = start + tabSpace.length;
+                e.preventDefault();
+                
+                output.log('Tab Hit');
                 break;
-            default:
+            case 'Enter':
+                if (editorOnFocus) {
+                    output.focus();
+                    editorOnFocus = !editorOnFocus;
+                } else {
+                    src.focus();
+                    editorOnFocus = !editorOnFocus;
+                    break;
+                }
+                console.log('Code Compiling Started');
+                
+                mainOutput += interpret(src.value);
+                document.getElementById('console').innerText = mainOutput;
+                
+                console.log('Code Compiling Ended');
+                console.log(mainOutput);
+                
+                
+                
                 break;
         }
     });
+    
 });
